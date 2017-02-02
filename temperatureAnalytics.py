822c04b3c100797d7cae151e9a64e5a40ed546d6
Joshua Malloy <a30088305@mcla.edu>
2017-02-02 14:19:57 +0000

    revised temp

--- temperatureAnalytics.py
+++ temperatureAnalytics.py
@@ -50,14 +50,26 @@ def main():
     percent = raw_input("Please enter the percent you want to analysis (e.g. 70  for 70%) ")
     stop1 = int((float(percent) / 100) * 116)
     start2 = stop1
+    
+    #reads infile
     temp = readTemps()
+    
+    #gets average
     average1 = getAve(temp, start1, stop1)
     average2 = getAve(temp, start2, stop2)
+    
+    #gets number of times a positive temp occurs
     posTemp1 = count(temp,start1,stop1)
     posTemp2 = count(temp,start2,stop2)
+    
+    #gets the starting point for last half to analysis
     lastYears = 116 - stop1
+    
+    #determines what the percent is being used
     firstPercent = float(percent) 
     secondPercent = 100 - float(percent)
+    
+    #displays all the data used first: the user selected percentage points then with the data of first section and then second
     print "You have selected the first percent of %s and second half percent of %s.\n" % (firstPercent , secondPercent)
     printOut("first",stop1, average1, posTemp1)
     printOut("second",lastYears, average2, posTemp2)
