6a4bdb460fa1b0148b46b1b869989c95daa03492
Joshua Malloy <a30088305@mcla.edu>
2017-03-21 16:06:19 +0000

    both programs with commits

--- perfectShuffle.py
+++ perfectShuffle.py
@@ -1,25 +1,32 @@
-import Epic
+import Epic # Gets input of an int
+
+#variables
 rank = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 suit = ['Clubs', 'Hearts', 'Diamonds', 'Spades']
 
+#This creats deck of cards
 def buildDeck(rank, suit):
     deck = []
-    for i in range(13):
-        for j in range(4):
+    for i in range(13): # this is for the rank of cards
+        for j in range(4): # this is for the suit of the cards
             deck.append(rank[i] + ' of ' + suit[j])
     return deck
     
 #shuffles the deck
 def shuffle(deck):
+#Variables
      newDeck = []
+     
 # creates the two deck halves
      firstHalf = deck[0:26]
      secondHalf = deck[26:]
+     
 # this takes one card from each deck and goes back an forth
      for i in range(26):
             newDeck.append(firstHalf[i])
             newDeck.append(secondHalf[i])
      return newDeck
+     
 #deals player the top five cards
 def deal(deal):
         return deal[0:5]
@@ -27,16 +34,23 @@ def deal(deal):
 def main():
 # variables
     shuffling = 2 # this needs to be two because we start at 0 in the 
-    # array we shuffle once before going into loop
+                  # array we shuffle once before going into loop
     d = buildDeck(rank,suit)
     s = shuffle(d)
+    
 # gets the number of times  users wants to shuffle
     timesShuffle = Epic.userInt("How many times do you want to shuffle? ")
+    
 # this shuffles the deck 
     while shuffling <= timesShuffle:     
         s = shuffle(s)
         shuffling = shuffling + 1
+        
+# creates users hand of 5 cards after calling deal
     hand = deal(s)
+    
 #prints users hand
     print hand
+    
+#calls main
 main()
--- randomShuffle.py
+++ randomShuffle.py
@@ -1,49 +1,61 @@
-import Epic
-import random
+import random 
+
+# Variables
 rank = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 suit = ['Clubs', 'Hearts', 'Diamonds', 'Spades']
 
 # this section builds the deck of cards
 def buildDeck(rank, suit):
     deck = []
-    for i in range(13):
-        for j in range(4):
+    for i in range(13): # this is for the rank of the cards
+        for j in range(4): # this is for the suit of the cards
             deck.append(rank[i] + ' of ' + suit[j])
     return deck
     
 #shuffles the deck
 def shuffle(deck):
+# variables
      newDeck = []
      randShuffle = 0
+     
 #cuts the deck in half
      firstHalf = deck[0:26]
      secondHalf = deck[26:]
 #this calls the halfShuffle to shuffle each half separately 
      newFirst = halfShuffle(firstHalf)
      newSecond = halfShuffle(secondHalf)
+     
 #this adds one card from each half deck to the newDeck
      for i in range(26):
             newDeck.append(newFirst[i])
             newDeck.append(newSecond[i])
      return newDeck
      
-#deals player the top five cards
+#deals player the top five cards in the deck
 def deal(deal):
         return deal[0:5]
 
 #this shuffles half a deck         
 def halfShuffle(half):
-     used = random.choice(half)
+
+# Variables
+     used = random.choice(half) # places random card into used
      cardsUsed = []
-     while len(cardsUsed) < 26:
+
+# Goes through loop until all cards are placed into deck
+     while len(cardsUsed) < 26: 
+
+# Places card into cardsUsed if card is not already in cardsUsed
         if not used in cardsUsed:
             cardsUsed.append(used)
+# gets a new card 
         else:
             used = random.choice(half)
+            
      return cardsUsed
     
 def main():
-# variables
+#this section creates deck, shuffles it, and deals a hand out to a single player
     d = buildDeck(rank,suit)
     s = shuffle(d)
     hand = deal(s)
