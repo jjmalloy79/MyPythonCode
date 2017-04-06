24848c4673e77c713e32279c9576b5e2d586a2b0
Joshua Malloy <a30088305@mcla.edu>
2017-04-06 15:29:51 +0000

    json updated

--- Json/Classes.py
+++ Json/Classes.py
@@ -19,10 +19,12 @@ def professor(classes):
     dataClass = []
     strG = ""
     dataGrade = []
+    profName = []
     count = 0
 # gets professor user wants to find what classes and grades they got
     name = Epic.userString("Enter a Professors name:")
     for classinfo in classes:
+        profName.append(classinfo["Name"])
         count = count + 1
         if classinfo["Name"].upper() == name.upper():
 # this gets each class and stores it into an array to printout
@@ -36,21 +38,45 @@ def professor(classes):
                 dataGrade.append(strG) 
 # if the professor is not found
         if classinfo["Name"].upper() != name.upper() and count == len(classes):
+# print out that the professor is not in the json file
             print "Sorry!! That professor was not found."
+            profList(profName)
+# calls printout if professor was found in json file
     printOut(dataClass,dataGrade)
-    
+# gets professors listed if user enters wrong data
+def profList(proList):
+# varables
+    counter = 0
+# prints out a title for the professor list
+    print "List of Professors"
+# loops through and prints out all professors in the json file
+    while counter < len(proList):
+        print proList[counter]
+        counter = counter + 1
 # Prints out data with class then grade
+
 def printOut(dataClass,dataGrade):
 # varables
     counter = 0
+# loops through each array and makes class and grad come up on same line
     while counter < len(dataClass):
-        print dataClass[counter] + ":     " + dataGrade[counter]
+# prints out the data
+        print "'%-10s   %s'" % (dataClass[counter],dataGrade[counter])
         counter = counter + 1
 
 def main():
-    readIn()
-
-               
-                
-                
+#varables
+    run = True
+# loops until user wants to stop
+    while run:
+# calls read in file to read in json file
+        readIn()
+# finds out if user wants to run again
+        runAgain = Epic.userString("Search again? (y or n)")
+# determines if user wants to run again or not
+        if runAgain == "y":
+            continue
+        else:
+            run = False
+# calls main function
 main()
